9f01336795854cd625d4f7e0c245bd29198ee4e7
mzaldirizaldi <mzaldirizaldi@gmail.com>
2023-06-12 21:30:51 +0700

    update

--- covidiagapi.py
+++ covidiagapi.py
@@ -33,7 +33,7 @@ def health_check():
 
 @app.route('/', methods=['POST'])
 def home():
-    # get data from POST
+    # Get data from POST
     input_names = ['breath_input', 'fever_input', 'dry_input', 'sore_input', 'running_input', 'asthma_input',
                    'chronic_input', 'headache_input', 'heart_input', 'diabetes_input', 'hyper_input',
                    'fatigue_input', 'gastro_input', 'abroad_input', 'contact_input', 'attend_input',
@@ -42,16 +42,16 @@ def home():
     input_values = {name: int(value) for name, value in request.form.items() if name in input_names and value.isdigit()}
 
     try:
-        # filter and convert to numpy array and reshape to one instance only
+        # Filter and convert to numpy array and reshape to one instance only
         integer_values = list(input_values.values())
         input_data = np.asarray(integer_values).reshape(1, -1)
 
-        # feed to model and predict
+        # Feed to model and predict
         pred_result_proba = model.predict_proba(input_data)
         pred_result = np.argmax(pred_result_proba)
         pred_result_proba = pred_result_proba[0, pred_result] * 100
 
-        # return result as json
+        # Return result as json
         return jsonify({'pred_result_proba': str(pred_result_proba), 'pred_result': str(pred_result)})
     except (ValueError, KeyError) as ex:
         logger.error(f"Invalid input data: {str(ex)}")
