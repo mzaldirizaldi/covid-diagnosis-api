281d63b077423608322b5c2aad53785737bb8c2d
mzaldirizaldi <mzaldirizaldi@gmail.com>
2023-06-12 21:37:31 +0700

    update

--- covidiagapi.py
+++ covidiagapi.py
@@ -61,6 +61,6 @@ def home():
 if __name__ == '__main__':
     try:
         logger.info("Starting the application.")
-        app.run(host='0.0.0.0', port=8080, debug=False)
+        app.run(host='0.0.0.0', port=8080)
     except Exception as e:
         logger.error(f"An error occurred while running the application: {str(e)}")
