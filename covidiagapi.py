18584fb0791d56ffaebc748ecc35de6ae8ac8fa7
mzaldirizaldi <mzaldirizaldi@gmail.com>
2023-06-13 11:46:44 +0700

    update

--- covidiagapi.py
+++ covidiagapi.py
@@ -68,7 +68,7 @@ def health_check():
 
 @app.route('/', methods=['POST'])
 def home():
-    # Get data from POST request
+    # Get data from POST request method
     input_names = [
         'breath_input', 'fever_input', 'dry_input', 'sore_input', 'running_input', 'asthma_input',
         'chronic_input', 'headache_input', 'heart_input', 'diabetes_input', 'hyper_input',
